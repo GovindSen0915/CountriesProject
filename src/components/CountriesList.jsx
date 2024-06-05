e12b321aa2e2a770208915210ccc5e9cc564fbb9
Govind Sen <govindsen0915@gmail.com>
2024-06-05 18:26:52 +0530

    feat: add react-router-dom

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import countriesData from '../../countriesData'
-import CountryCard from './CountryCard';
+import React, { useEffect, useState } from "react";
+// import countriesData from '../../countriesData'
+import CountryCard from "./CountryCard";
 
-function CountriesList({query}) {
-    return (
-        <>
-          <div className="countries-container">
-            {countriesData
-              .filter((country) =>
-                country.name.common.toLowerCase().includes(query)
-              )
-              .map((country) => {
-                return (
-                  <CountryCard
-                    key={country.name.common}
-                    name={country.name.common}
-                    flag={country.flags.svg}
-                    population={country.population}
-                    region={country.region}
-                    capital={country.capital?.[0]}
-                  />
-                )
-              })}
-          </div>
-        </>
-      )
+function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesData(data);
+      })
+  }, []);
+
+  return (
+    <>
+      <div className="countries-container">
+        {countriesData
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
 }
 
-export default CountriesList
+export default CountriesList;
